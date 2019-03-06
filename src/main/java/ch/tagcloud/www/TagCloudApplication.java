2aad7d070aebd96a18e47cba17709b69b81f50e5
nussbi <nussbi@LAPTOP-2C5RN20E.zhaw.ch>
2019-03-06 14:53:47 +0100

    Initiale Erstellung

---
+++ src/main/java/ch/tagcloud/www/TagCloudApplication.java
@@ -0,0 +1,13 @@
+package ch.tagcloud.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagCloudApplication.class, args);
+	}
+
+}
