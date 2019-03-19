121235cede03c7c64f8c4ab48f207308f65cefb6
nussbluk <nussbluk@students.zhaw.ch>
2019-03-19 21:41:49 +0100

    Registration und Login hinzugefügt (inkomplett), diverse Einstellungen

--- src/main/java/ch/tagcloud/www/TagCloudApplication.java
+++ src/main/java/ch/tagcloud/www/TagCloudApplication.java
@@ -2,12 +2,17 @@ package ch.tagcloud.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TagCloudApplication {
+public class TagCloudApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TagCloudApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(TagCloudApplication.class, args);
-	}
-
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TagCloudApplication.class, args);
+    }
 }
